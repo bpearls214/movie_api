55c39a3d2f07dfc05902038d57f2eef9c6311f23
bpearls214 <48865572+bpearls214@users.noreply.github.com>
2019-12-03 19:21:29 -0500

    Added models.js and updated all endpoints

--- index.js
+++ index.js
@@ -5,195 +5,206 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+const Directors = Models.Director;
+const Genres = Models.Genre;
+
+mongoose.connect('mongodb://localhost:27017/cineMeDB', {useNewUrlParser: true});
+
 app.use(bodyParser.json());
 
 app.use(morgan('common'));
 
-let TopMovies = [
-{
-  title : 'Bridesmaids',
-  description: 'Competition between the maid of honor and a bridesmaid, over who \
-   is the bride\'s best friend, threatens to upend the life of an out-of-work \
-   pastry chef.',
-  comedyLevel: 'insanely hilarious',
-  genre : 'Comedy',
-   director : 'Paul Feig'
-  },
-  {
-    title : 'Step Brothers',
-    description : 'Two co-dependent high school seniors are forced to deal \
-    with separation anxiety after their plan to stage a booze-soaked party goes awry.',
-    comedyLevel : 'stupidly funny',
-    genre : 'Comedy',
-     director : 'Greg Mottola'
-  },
-  {
-    title : 'Meet the Parents',
-    description : 'Male nurse Greg Focker meets his girlfriend\'s parents before \
-    proposing, but her suspicious father is every date\'s worst nightmare.',
-    comedyLevel : 'Really Focking good',
-    genre : 'Comedy',
-     director : 'Jay Roach'
-  },
-  {
-    title : 'Forgetting Sarah Marshell',
-    description : 'Devastated Peter takes a Hawaiian vacation in order to deal with \
-    the recent break-up with his TV star girlfriend, Sarah. Little does he know, \
-    Sarah\'s traveling to the same resort as her ex - and she\'s \
-    bringing along her new boyfriend.',
-    comedyLevel : 'American treasure',
-    genre : 'Romantic Comedy',
-     director : 'Nicholas Stroller'
-  },
-  {
-    title : 'Twilight',
-    description : 'Bella Swan moves to Forks and encounters Edward Cullen, a gorgeous boy with a secret.',
-    comedyLevel : 'soul sucking',
-    genre :  'Drama',
-    director : 'Catherine Hardwicke'
-  },
-  {
-    title : 'Titanic',
-    description : 'A seventeen-year-old aristocrat falls in love with a kind but \
-    poor artist aboard the luxurious, ill-fated R.M.S. Titanic.',
-    comedyLevel : 'People fight over doors ability to support more than one person',
-    genre : 'Drama',
-    director : 'James Cameron'
-  }
-];
-
-let Genres = [
-  {
-  name : 'Drama',
-  description : 'Should contain numerous consecutive scenes of characters portrayed\
-  to effect a serious narrative throughout the title\. This can be exaaggerated\
-  upon to produce a melodrama\. Subjective\.',
-  comedyLevel: 'potential humor undercurrents'
-},
-{
-  name : 'Romantic Comedy',
-  description : 'A romantic comedy film should just be classified as comedy\
-  however society likes to classify things that don\'t always require a new \
-  classification.  Romantic comedies typically have an undercurrent or secondary \
-  story line that is romantic in nature, making these comedies "Romantic Comedies" \
-  and often branded as comedy for females.',
-  comedyLevel: 'literally comedy'
-},
-{
-   name : 'Comedy',
-   description : 'A comedy film is a genre of film in which the main emphasis is \
-   on humour. These films are designed to make the audience laugh through amusement and most often \
-   work by exaggerating characteristics for humorous effect.',
-   comedyLevel: 'literally comedy'
- },
-];
-
-let Directors = [
-  {
-    name : 'James Cameron',
-    born : 'August 16, 1954'
-  },
-  {
-    name : 'Catherine Hardwicke',
-    born : 'October 21, 1955'
-  },
-  {
-    name : 'Nicholas Stroller',
-    born : 'March 19, 1976'
-  },
-  {
-    name : 'Jay Roach',
-    born : 'June 14, 1957'
-  },
-  {
-    name : 'Paul Feig',
-    born : 'September 17, 1962'
-  }
-];
-
-let Users = [
-  {
-    id : "5d9d82069bc166069f68524d",
-    favoriteMovies : [],
-    username : "moviebuff74",
-    password : "securepassword89",
-    email : "example123456@gmail.com",
-    birthday : "1974-02-14"
-  }
-];
-
 // GET requests
-app.get('/', (req, res) => {
-  res.send('Welcome to my app!');
-});
-
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
-});
-
 // get a list of all top movies
 app.get('/movies', (req, res) => {
-  res.json(TopMovies)
+  Movies.find().then(movies => res.json(movies));
 });
 
 // get movie info by movie title
-app.get('/movies/:title', (req,res) => {
-  res.json(TopMovies.find( (movie) =>
-    {return movie.title === req.params.title }));
+app.get('/movies/:Title', function(req, res) {
+  Movies.findOne({ Title : req.params.Title })
+  .then(function(movie) {
+    res.json(movie)
+  })
+  .catch(function(err) {
+    console.error(err);
+    res.status(500).send("Error: " + err);
   });
+});
 
 // get genre info by genre name
-app.get('/genre/:name', (req,res) => {
-  res.json(Genres.find ( (genre) =>
-    {return genre.name === req.params.name }));
+app.get('/genres/:Name', function(req, res) {
+  Genres.findOne({ Name : req.params.Name })
+  .then(function(genre) {
+    res.json(genre)
+  })
+  .catch(function(err) {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
 });
 
 // Get director info by director name
-app.get('/director/:name', (req, res) => {
-  res.json(Directors.find ( (director) =>
-    {return director.name === req.params.name}));
+app.get('/directors/:Name', function(req, res) {
+  Directors.findOne({ Name : req.params.Name })
+  .then(function(director) {
+    res.json(director)
+  })
+  .catch(function(err) {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
+
+// Get list of all users
+app.get('/users', function(req,res) {
+  Users.find ()
+  .then(function(users) {
+    res.status(201).json(users)
+  })
+    .catch(function(err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 // Get full user info by username
-app.get('/users/:username', (req,res) => {
-  res.json(Users.find ( (user) =>
-    {return user.username === req.params.username}));
+app.get('/users/:Username', function(req, res) {
+  Users.findOne({ Username : req.params.Username })
+  .then(function(user) {
+    res.json(user)
+  })
+  .catch(function(err) {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
 });
 
 // PUT requests
 // update users information
-app.put('/users/:username/:password/:email/:birthday', (req, res) => {
-  res.send('User information updated.');
+app.put('/users/:Username', function (req, res) {
+  Users.findOneAndUpdate({ Username : req.params.Username },
+  { $set :
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday,
+      Address: {
+        Street: req.body.Street,
+        City: req.body.City,
+        State: req.body.State
+      }
+  }},
+  { new : true }, //this line is to make sure updated doc is returned
+  function(err, updatedUser) {
+    if(err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    } else {
+      res.json(updatedUser)
+    }
+  })
 });
 
 // POST requests
-// adds data for a new user
-app.post('/users', (req, res) => {
-  let newUser = req.body;
-
-  if (!newUser.username) {
-    res.status(400).send('Missing username in request body');
-  } else {
-    newUser.id = uuid.v4();
-    Users.push(newUser);
-    res.send(newUser);
-  }
+// adds a user
+/* Anticipated JSON format:
+{
+  ID : Integer,
+  Username : String,
+  Password : String,
+  Email : String,
+  Address : {
+  Street : String,
+  City : String,
+  State : String
+}
+  Birthday : Date
+} */
+app.post('/users', function(req, res) {
+  Users.findOne({ Username : req.body.Username })
+  .then(function(user) {
+    if (user) {
+      return res.status(400).send(req.body.Username + " already exists!");
+    } else {
+      Users
+      .create({
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+        Address: {
+          Street: req.body.Street,
+          City: req.body.City,
+          State: req.body.State
+        }
+      })
+      .then(function(user) {res.status(201).json(user)
+  })
+      .catch(function(error) {
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      })
+    }
+  }).catch(function(error) {
+    console.error(error);
+    res.status(500).send("Error: " + error);
+  });
 });
 
 //adds movie to favorites for user
-app.post('/favoriteMovies/:username/:title', (req, res) => {
-  res.send('Favorite movie added to user profile.');
+app.post('/users/:Username/Movies/:MovieID', function(req, res) {
+  Users.findOneAndUpdate(
+    { Username : req.params.Username },
+    {$push : { SavedMovies : req.params.MovieID } },
+    {new : true }, // ensures updated doc is returned
+    function(err, updatedUser) {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    } else {
+      res.json(updatedUser)
+    }
+  })
 });
 
 // DELETE requests
 // deletes a movie from a users favorites by username
-app.delete('/favoriteMovies/:username/:title', (req, res) => {
-  res.send('Movie was sucessfully removed from your list of favorite movies.');
-});
+app.delete('/users/:Username/Movies/:MovieID', function(req, res) {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    { $pull : { SavedMovies : req.params.MovieID } },
+    { new : true }, //ensures updated doc is returned
+    function(err, updatedUser) {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    } else {
+      res.json(updatedUser)
+    }
+  })
+  });
 
-// deletes a user from the registry of users
-app.delete('/users/:username', (req, res) => {
-  res.send('User account was successully deleted.');
+// deletes a user by username
+app.delete('/users/:username', function(req, res) {
+  Users.findOneAndRemove({ Username: req.params.Username})
+  .then(function(user) {
+    if (!user) {
+      res.status(400).send(req.params.Username + " was not found");
+    } else {
+      res.status(200).send(req.params.Username + " was deleted.");
+    }
+    })
+    .catch(function(err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 
---
+++ models.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+var genreSchema = mongoose.Schema({
+  Name: {type: String, required: true},
+  Description: {type: String, required: true}
+});
+
+var directorSchema = mongoose.Schema({
+  Name: {type: String, required: true},
+  Bio: {type: String},
+  Birthyear: {type: String, required: true},
+  Deathyear: {type: String}
+});
+
+var movieSchema = mongoose.Schema({
+  Title: {type: String, required: true},
+  Description: {type: String, required: true},
+  Genre: [{type: mongoose.Schema.Types.ObjectId, ref:'Genres'}],
+  Director: [{type: mongoose.Schema.Types.ObjectId, ref:'Directors'}],
+  ComedyRating: {type: String}
+});
+
+var userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  Address: {
+    Street: String,
+    City: String,
+    State: String
+  },
+  SavedMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+var Genre = mongoose.model('Genre', genreSchema);
+var Director = mongoose.model('Director', directorSchema);
+var Movie = mongoose.model('Movie', movieSchema);
+var User = mongoose.model('User', userSchema);
+
+module.exports.Genre = Genre;
+module.exports.Director = Director;
+module.exports.Movie = Movie;
+module.exports.User = User;
